5de62783b39ecd1da1a4696b3b9bd24b12f4d81f
kiennguyen2403 <kientqk2403@example.com>
2023-10-16 20:15:32 +1100

    adding test

---
+++ test/test.js
@@ -0,0 +1,31 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const assert = require('assert');
+
+describe('Integration Test', function () {
+    let driver;
+
+    before(async function () {
+        // Set up the Selenium WebDriver instance
+        driver = new Builder().forBrowser('chrome').build();
+    });
+
+    after(async function () {
+        // Clean up after the tests
+        await driver.quit();
+    });
+
+    it('Check the title of the page', async function () {
+        // Navigate to your application's URL
+        await driver.get('http://localhost:3000'); // Replace with your app's URL
+
+        // You can add assertions to verify the presence of specific elements
+        const element = await driver.findElement(By.className('intro'));
+        assert.ok(await element.isDisplayed(), 'A decentralised trading platform');
+
+        // You can also perform interactions with your app
+
+        // Add more assertions or interactions as needed
+    });
+    done();
+    // Add more test cases as needed
+});
